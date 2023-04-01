7598341731164c1a880ed7aa1d2786551d0fd292
Shiho Nagano <ex3jsb.geneg@gmail.com>
2023-04-01 14:02:31 -0700

    commented out example ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -6,9 +6,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  example: exampleRouter,
-});
+export const appRouter = createTRPCRouter({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
