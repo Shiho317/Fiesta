baeeae018cea1dbd48526598c63b37fd61fe584f
Shiho Nagano <ex3jsb.geneg@gmail.com>
2023-08-22 11:35:48 +0900

    deleted discord variables

--- src/env.mjs
+++ src/env.mjs
@@ -19,8 +19,8 @@ const server = z.object({
     process.env.VERCEL ? z.string().min(1) : z.string().url()
   ),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  // DISCORD_CLIENT_ID: z.string(),
+  // DISCORD_CLIENT_SECRET: z.string(),
   EMAIL_SERVER: z.string(),
   EMAIL_FROM: z.string(),
   JWT_SECRET_KEY: z.string(),
@@ -45,8 +45,8 @@ const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   EMAIL_SERVER: process.env.EMAIL_SERVER,
   EMAIL_FROM: process.env.EMAIL_FROM,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
